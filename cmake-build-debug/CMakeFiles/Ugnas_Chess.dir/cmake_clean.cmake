5bf3d3891263225568edfe3419af0055653cbe87
chorpus <corpus_e@outlook.com>
2021-05-07 17:04:12 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/Ugnas_Chess.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ugnas_Chess.dir/main.c.obj"
+  "Ugnas_Chess.exe"
+  "Ugnas_Chess.exe.manifest"
+  "Ugnas_Chess.pdb"
+  "libUgnas_Chess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ugnas_Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ defs.h
@@ -0,0 +1,8 @@
+//
+// Created by corpu on 07/05/2021.
+//
+
+#ifndef UGNAS_CHESS_DEFS_H
+#define UGNAS_CHESS_DEFS_H
+
+#endif //UGNAS_CHESS_DEFS_H
