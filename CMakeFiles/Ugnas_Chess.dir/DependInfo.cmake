376fb8a52275995c948005ac17a5b82056f7c77d
corpus96 <corpus_e@outlook.com>
2021-06-27 15:03:19 -0500

    Print move and print square

--- CMakeFiles/Ugnas_Chess.dir/DependInfo.cmake
+++ CMakeFiles/Ugnas_Chess.dir/DependInfo.cmake
@@ -1,21 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/attack.c" "/home/emmanuelcorpus/Documentos/Ugnas-Chess/CMakeFiles/Ugnas_Chess.dir/attack.c.o"
-  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/bitboard.c" "/home/emmanuelcorpus/Documentos/Ugnas-Chess/CMakeFiles/Ugnas_Chess.dir/bitboard.c.o"
-  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/board.c" "/home/emmanuelcorpus/Documentos/Ugnas-Chess/CMakeFiles/Ugnas_Chess.dir/board.c.o"
-  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/data.c" "/home/emmanuelcorpus/Documentos/Ugnas-Chess/CMakeFiles/Ugnas_Chess.dir/data.c.o"
-  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/hashkeys.c" "/home/emmanuelcorpus/Documentos/Ugnas-Chess/CMakeFiles/Ugnas_Chess.dir/hashkeys.c.o"
-  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/init.c" "/home/emmanuelcorpus/Documentos/Ugnas-Chess/CMakeFiles/Ugnas_Chess.dir/init.c.o"
-  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/main.c" "/home/emmanuelcorpus/Documentos/Ugnas-Chess/CMakeFiles/Ugnas_Chess.dir/main.c.o"
-  )
-set(CMAKE_C_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/attack.c" "CMakeFiles/Ugnas_Chess.dir/attack.c.o" "gcc" "CMakeFiles/Ugnas_Chess.dir/attack.c.o.d"
+  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/bitboard.c" "CMakeFiles/Ugnas_Chess.dir/bitboard.c.o" "gcc" "CMakeFiles/Ugnas_Chess.dir/bitboard.c.o.d"
+  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/board.c" "CMakeFiles/Ugnas_Chess.dir/board.c.o" "gcc" "CMakeFiles/Ugnas_Chess.dir/board.c.o.d"
+  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/data.c" "CMakeFiles/Ugnas_Chess.dir/data.c.o" "gcc" "CMakeFiles/Ugnas_Chess.dir/data.c.o.d"
+  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/hashkeys.c" "CMakeFiles/Ugnas_Chess.dir/hashkeys.c.o" "gcc" "CMakeFiles/Ugnas_Chess.dir/hashkeys.c.o.d"
+  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/init.c" "CMakeFiles/Ugnas_Chess.dir/init.c.o" "gcc" "CMakeFiles/Ugnas_Chess.dir/init.c.o.d"
+  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/io.c" "CMakeFiles/Ugnas_Chess.dir/io.c.o" "gcc" "CMakeFiles/Ugnas_Chess.dir/io.c.o.d"
+  "/home/emmanuelcorpus/Documentos/Ugnas-Chess/main.c" "CMakeFiles/Ugnas_Chess.dir/main.c.o" "gcc" "CMakeFiles/Ugnas_Chess.dir/main.c.o.d"
   )
 
 # Targets to which this target links.
--- defs.h
+++ defs.h
@@ -99,13 +99,26 @@ typedef struct {
 
 
 /*
-0000 0000 0000 0000 0000 0111 1111 -> From
-0000 0000 0000 0011 1111 1000 0000 -> To
-0000 0000 0011 1100 0000 0000 0000 -> Captured
-0000 0000 0100 0000 0000 0000 0000 -> EP
-0000 0111 1000 0000 0000 0000 0000 -> Promoted Piece
-0000 1000 0000 0000 0000 0000 0000 -> Castle
+0000 0000 0000 0000 0000 0111 1111 -> From 0x3F
+0000 0000 0000 0011 1111 1000 0000 -> To >> 7, 0x3F
+0000 0000 0011 1100 0000 0000 0000 -> Captured >> 14, 0xF
+0000 0000 0100 0000 0000 0000 0000 -> EP 0x40000
+0000 0000 1000 0000 0000 0000 0000 -> Pawn Start 0x80000
+0000 1111 0000 0000 0000 0000 0000 -> Promoted Piece >> 20, 0xF
+0001 0000 0000 0000 0000 0000 0000 -> Castle 0x1000000
 */
+
+#define FROMSQ(m) ((m) & 0x7F)
+#define TOSQ(m) (((m) >> 7) & 0x7F)
+#define CAPTURED(m) (((m) >> 14) & 0xF)
+#define PROMOTED(m) (((m) >> 20) & 0xF)
+
+#define MFLAGEP 0x40000
+#define MFLAGPS 0x80000
+#define MFLAGCA 0x1000000
+
+#define MFLAGCAP 0x7C000
+#define MFLAGPROM 0xF00000
 
 /* MACROS */
 
@@ -172,6 +185,10 @@ extern int CheckBoard(const S_BOARD *pos);
 
 //attack.c
 extern int SqAttacked(const int sq, const int side, const S_BOARD *pos);
+
+//io.c
+extern char *PrMove(const int move);
+extern char *PrSq(const int sq);
 
 #endif
 
